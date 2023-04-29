013b0e4c251c507f6b3081872b3714caf567d77d
Jacson <jacson.sasse@hotmail.com>
2023-04-29 17:21:56 -0300

    Iniciado a criação dos comportamentos do componente todo-list
    Criada a exibição das tasks e a exibição do background especial, quando não houver tasks
    
    Criado comportamento para apenas exibir o botão de apagar todas se houver tasks

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+// Interface
+import { TaskList } from '../../model/task-list';
+
 @Component({
-  selector: 'app-todo-list',
-  templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.scss']
+    selector: 'app-todo-list',
+    templateUrl: './todo-list.component.html',
+    styleUrls: ['./todo-list.component.scss'],
 })
 export class TodoListComponent {
-
+    public taskList: Array<TaskList> = [
+        { task: 'Minha nova task', checked: true },
+        { task: 'Minha nova task 2', checked: false },
+    ];
 }
